4aafa519bdc941096095b48f134add020205ee4a
零售平台-黄强 <qiang.huang@qeeka.com>
2019-12-23 13:49:23 +0800

    model

---
+++ src/components/ModelTest.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="model">
+    <input type="text" @input="changeInput" :value="defaultInput" />
+  </div>
+</template>
+<script lang="ts">
+import Vue from 'vue';
+import { Prop, Model, Component, Emit } from 'vue-property-decorator';
+
+@Component
+export default class ModelTest extends Vue {
+  @Model('input', { type: String }) private defaultInput?: string;
+
+  @Emit('input')
+  private changeInput(e: any): string {
+    return e.target.value;
+  }
+}
+</script>
